48ac52ba851b6e8dda6b70deceddf81164547a8b
Kayke Moreira <kayke.allogins@gmail.com>
2026-01-18 15:39:19 -0300

    Search onSearch event

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import "./styles.css";
 
-export default function SearchBar() {
+type Props = {
+  onSearch: (searchText: string) => void;
+};
+
+export default function SearchBar({ onSearch }: Props) {
+  const [text, setText] = useState<string>("");
+
+
+  function handleChange(event: any) {
+    setText(event.target.value);
+  }
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    onSearch(text);
+  }
+
+  function handleResetClick() {
+    setText("");
+    onSearch(text);
+  }
+
   return (
-    <form className="dsc-search-bar">
+    <form className="dsc-search-bar" onSubmit={handleSubmit}>
       <button type="submit">🔍︎</button>
-      <input type="text" placeholder="Nome do produto" />
-      <button type="reset">X</button>
+      <input value={text} type="text" placeholder="Nome do produto" onChange={handleChange}/>
+      <button onClick={handleResetClick}>X</button>
     </form>
   );
 }
--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -8,21 +8,28 @@ import * as productService from "../../../services/product-service";
 
 export default function Catalog() {
   const [products, setProducts] = useState<ProductDTO[]>([]);
+  
+  const [productName, setProductName] = useState<string>("");
 
   useEffect(() => {
-    productService.findAll().then((response) => {
+    productService.findPageRequest(0, productName).then((response) => {
       if (response.data) {
         const products = response.data.content;
         setProducts(products);
       }
     });
-  }, products);
+  }, [productName]);
+
+
+  function handleSearch(searchText: string) {
+    setProductName(searchText);
+  }
 
   return (
     <>
       <main>
         <section id="catalog-section" className="dsc-container">
-          <SearchBar />
+          <SearchBar onSearch={handleSearch} />
           <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
             {products.map((product) => (
               <CatalogCard key={product.id} product={product} />
