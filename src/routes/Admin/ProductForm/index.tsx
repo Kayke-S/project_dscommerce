00eb2cb851c5285947d9ca7e92461d8cc38f312e
Kayke Moreira <kayke.allogins@gmail.com>
2026-01-25 19:12:04 -0300

    ProductForm Layout

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,6 +1,62 @@
-export default function ProductForm() {
+import './styles.css';
 
-    return (
-        <h1>Formulário de produtos</h1>
-    );
+export default function ProductForm() {
+  return (
+    <main>
+      <section id="product-form-section" className="dsc-container">
+        <div className="dsc-product-form-container">
+          <form className="dsc-card dsc-form">
+            <h2>Dados do produtos</h2>
+            <div className="dsc-form-controls-container">
+              <div>
+                <input
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Nome"
+                />
+              </div>
+              <div>
+                <input
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Preço"
+                />
+              </div>
+              <div>
+                <input
+                  className="dsc-form-control"
+                  type="text"
+                  placeholder="Imagem"
+                />
+              </div>
+              <div>
+                <select className="dsc-select dsc-form-control" required>
+                  <option value="" disabled selected>
+                    Categorias
+                  </option>
+                  <option value="1">cat 1</option>
+                  <option value="2">cat 2</option>
+                  <option value="3">cat 3</option>
+                </select>
+              </div>
+              <div>
+                <textarea
+                  className="dsc-form-control dsc-textarea"
+                  placeholder="Descrição"
+                ></textarea>
+              </div>
+            </div>
+            <div className="dsc-product-form-buttons">
+              <button type="reset" className="dsc-btn dsc-btn-white">
+                Cancelar
+              </button>
+              <button type="submit" className="dsc-btn dsc-btn-blue">
+                Salvar
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </main>
+  );
 }
